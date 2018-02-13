3b32b3c1669097cb961224f8575738e976e4c5cc
Taras Kushnir <kushnirTV@gmail.com>
2018-02-13 13:53:49 +0000

    Cosmetic tweaks for codecov script

--- src/xpiks-tests/get_code_cov.sh
+++ src/xpiks-tests/get_code_cov.sh
@@ -2,8 +2,12 @@
 
 echo "Calculating code coverage for directory: $1"
 
-for filename in `find $1 -type f \( -name "*.cpp" -not -name "moc_*.cpp" \)`; 
+pushd "$1"
+
+for filename in `find . -type f \( -name "*.cpp" -not -name "moc_*.cpp" \)`; 
 do
-    echo "Processing $filename"
+    # echo "Processing $filename"
     gcov-5 -o . "$filename" > /dev/null; 
 done
+
+popd
