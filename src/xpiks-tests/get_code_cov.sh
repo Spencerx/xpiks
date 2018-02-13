cf7fa843eaa1e8ef040ba553636fa4ca620d809a
Taras Kushnir <kushnirTV@gmail.com>
2018-02-13 13:30:59 +0000

    More fixes for codecov

--- src/xpiks-tests/get_code_cov.sh
+++ src/xpiks-tests/get_code_cov.sh
@@ -2,7 +2,7 @@
 
 echo "Calculating code coverage for directory: $1"
 
-for filename in `find $1 | egrep '\.cpp'`; 
+for filename in `find $1 -type f \( -name "*.cpp" -not -name "moc_*.cpp" \)`; 
 do
     echo "Processing $filename"
     gcov-5 -o . "$filename" > /dev/null; 
