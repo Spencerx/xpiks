d1b4a65e72071c5af3c8ef1cac5e87b4f2f34404
Taras Kushnir <505555+ribtoks@users.noreply.github.com>
2018-04-30 21:59:34 +0300

    Update checkout_deps_windows.bat

--- vendors/checkout_deps_windows.bat
+++ vendors/checkout_deps_windows.bat
@@ -7,6 +7,5 @@ cd xpiks-deps
 git init
 git remote add origin https://bitbucket.org/ribtoks/xpiks-deps.git
 
-git fetch origin 76c80a64ff15d6f67cabf37e4120d1a0c19a5e31:refs/remotes/origin/foo-commit
-
-git reset --hard FETCH_HEAD
+git fetch --depth=30 origin master
+git merge 76c80a64ff15d6f67cabf37e4120d1a0c19a5e31
